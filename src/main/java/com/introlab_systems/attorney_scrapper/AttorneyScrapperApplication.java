e89c55ee306331de8e4b8d1d82b048f0a994d449
Georgii Sorokin <44945227+dante7222@users.noreply.github.com>
2020-09-29 13:57:04 +0300

    Init

---
+++ src/main/java/com/introlab_systems/attorney_scrapper/AttorneyScrapperApplication.java
@@ -0,0 +1,13 @@
+package com.introlab_systems.attorney_scrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttorneyScrapperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttorneyScrapperApplication.class, args);
+    }
+
+}
